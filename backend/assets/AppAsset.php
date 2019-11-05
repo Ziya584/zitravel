de941a44179eedfdec2f178c74d916ac676c8ca9
ziya.mammadov.r@gmail.com <ziya584@bitbucket.org>
2019-11-05 19:45:27 +0300

    Настроил новый лэйоут для админки и добавил необходимые переменные на модель User

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,12 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/style4.css',
     ];
     public $js = [
+    	'https://code.jquery.com/jquery-1.12.0.min.js',
+		'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+		'js/default.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- common/models/User.php
+++ common/models/User.php
@@ -28,7 +28,11 @@ class User extends ActiveRecord implements IdentityInterface
     const STATUS_INACTIVE = 9;
     const STATUS_ACTIVE = 10;
 
-
+	public $password_hash;
+	public $auth_key;
+	public $verification_token;
+	public $created_at;
+	public $updated_at;
     /**
      * {@inheritdoc}
      */
