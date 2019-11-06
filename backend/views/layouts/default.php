17b91afad9920daac047f696223a0df11da1042c
ziya.mammadov.r@gmail.com <ziya584@bitbucket.org>
2019-11-06 04:16:50 +0300

    Подправил недочёты отображения контента

--- backend/views/layouts/default.php
+++ backend/views/layouts/default.php
@@ -95,19 +95,16 @@ AppAsset::register($this);
 		<nav class="navbar navbar-default">
 			<div class="container-fluid">
 
-				<div class="navbar-header">
+				<div class="navbar-header ">
 					<button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
 						<i class="glyphicon glyphicon-align-left"></i>
-						<span>Toggle Sidebar</span>
+						<span></span>
 					</button>
 				</div>
 
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Page</a></li>
-						<li><a href="#">Page</a></li>
-						<li><a href="#">Page</a></li>
-						<li><a href="#">Page</a></li>
+
 					</ul>
 				</div>
 			</div>
